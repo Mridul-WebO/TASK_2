b7d32bdb3660a150093defae4a51fe4096f12179
mriduls <mriduls.webosmotic@gmail.com>
2024-01-12 10:10:17 +0530

    first commit

---
+++ file1.js
@@ -0,0 +1,62 @@
+// // binary2decimal conversion class
+
+// class conversion {
+//   b2d(b) {
+//     return parseInt(b, 2).toString(10);
+//   }
+//   d2b(d) {
+//     return parseInt(d, 10).toString(2);
+//   }
+// }
+
+// const convert = new conversion();
+
+// const result = {
+//   a: 1,
+//   b: 2,
+//   add: function (c, d) {
+//     document.write(this.a + this.b);
+//   },
+// };
+
+// const data1 = {
+//   a: 5,
+//   b: 5,
+//   sub: function (c, d) {
+//     return this.a + this.b + c + d;
+//   },
+// };
+
+// const data2 = {
+//   a: 15,
+//   b: 25,
+// };
+
+// // call
+
+// // console.log(result.add(1, 2));
+// // console.log(typeof result.add.call(data1, 1, 2));
+// // console.log(result.add.apply(data1, [1, 2]));
+
+// setTimeout(
+//   () => {
+//     result.add.call(data1);
+//   },
+
+//   // console.log(result.add.bind(data1)(1,2));
+//   6000
+// );
+// // console.log(result.add.call(data2));
+
+// // apply
+
+// // console.log(data2..apply());
+
+// const b = 2;
+
+// (function () {
+//   console.log(c);
+//   var c = 2;
+// })();
+
+// call , apply , bind
---
+++ file2.js
@@ -0,0 +1,11 @@
+export class Demo {
+  constructor(name, age) {
+    (this.name = name), (this.age = age);
+  }
+
+  result() {
+    document.write(this.name);
+  }
+}
+const fname = "file2";
+export default fname;
